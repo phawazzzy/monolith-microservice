75a04c76f5fb62ed1d3d441e9eea1f496e229cd8
Fawas Kareem <phawazzzy@gmail.com>
2022-10-09 12:56:22 +0100

    updated api host endpoint

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aeaefdfcc11dc45ba97d16da142ba25e-1171355223.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
